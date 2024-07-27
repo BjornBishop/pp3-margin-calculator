a103ac28ffe2306ee7c9b1ba8a61e616429cc589
Bjorn Bishop <bjornbishop@gmail.com>
2024-07-27 10:15:42 +0000

    refactored get_bill and get_contract_duration functions. Added get burdens function

--- run.py
+++ run.py
@@ -14,44 +14,130 @@ SHEET = GSPREAD_CLIENT.open('margin-calculator')
 
 margins = SHEET.worksheet('margins')
 
+def validate_bill_rate(bill_data):
+    """
+    Inside the TRY, converts the input data to an integer.
+    Raise a ValueError if data cannot be converted
+    or if multiple values are input.
+    """
+    try:
+        int(bill_data)
+    except ValueError as e:
+        print(f'Invalid input {e}, please try again\n')
+        return False
+    return True
+
+def validate_contract(contract_duration):
+    """
+    Inside the TRY, convert the input data into an integer. 
+    Raise a ValueError if data can not be converted 
+    or if multiple values are input
+    """
+    try:
+        int(contract_duration)
+    except ValueError as e:
+        print(f'Invalid input {e}, please try again\n')
+        return False
+    return True
+
+def validate_burdens(client_burdens):
+    """
+    inside the TRY, conver the input data into a floating integer.
+    Raise a ValueError if data can not be converted
+    or if multiple values are input
+    """
+    try:
+        original_float = float(client_burdens)
+        rounded_float = round(origin_float, 2)
+        return rounded_float
+    except ValueError as e:
+        print('Invalid input {e}, please try again \n')
+        return False
+    return True
+
 def get_bill_rate():
     """
     Get bill rate data from the user.
-    run while loop to collect valid data from the user 
-    via the terminal, which must be an integer. The loop will 
-    repeatedly request data until the input is valid
+    Run a while loop to collect valid data from the user
+    via the terminal, which must be an integer. The loop will
+    repeatedly request data until the input is valid.
     """
     while True:
-        print("Please enter hourly bill rate data from client")
-        print("Data should be in number format. No need to add '/h'")
-        print("Example: 800 or 1000\n")
+        print("Please enter hourly bill rate data from the client.")
+        print("Data should be in number format. No need to add '/h'.")
+        print("Example: 800 or 1000 \n")
 
-        bill_data = input('Enter Bill Rate here: \n')
+        bill_data = input('Enter Bill Rate here: ')
 
-        if validate_data(bill_data):
-            print("Data input valid")
+        if validate_bill_rate(bill_data):
+            print("Data input valid \n")
             break
     return bill_data
-    get_contract_duration()
 
 def get_contract_duration():
     """
-    Get the contract duration from the user. 
-    runs while loop to collect valid data from the user
-    via the terminal, which must be an integer. The loop will 
-    repeatedly request data until the input is valid
+    Get the contract duration from the user.
+    Run a while loop to collect valid data from the user
+    via the terminal, which must be an integer. The loop will
+    repeatedly request data until the input is valid.
     """
     while True:
-        print("Please enter the duration of the contract")
+        print("Please enter the duration of the contract.")
         print("Data should be in number format. No need to add 'months' etc.")
-        print("Example: 6 or 12")
+        print("Example: 6 or 12 \n")
 
-        contract_duration = input('Enter Contract Duration here: \n')
+        contract_duration = input('Enter Contract Duration here: ')
 
-        if validate_data(contract_duration):
-            print("Data input valid")
+        if validate_contract(contract_duration):
+            print("Data input valid \n")
             break
     return contract_duration
 
-start = get_bill_rate()
-print(start)
+def get_burdens():
+    """
+    Gets the associated burdens held with the client contract 
+    Run a while loop to collect valid data from the user 
+    via the terminal, which much be a float. The loop will 
+    repeatedly request data until the input is valid 
+    """
+    print("Please enter the burdens held with the client.")
+    print("data should be in number format, no need to add '%'")
+    print("Example: 2.5")
+
+    client_burdens = input('Enter Client Burdens here: ')
+
+    if validate_burdens(client_burdens):
+        print("Data input valid \n")
+        break
+    return client_burdens
+
+def get_liabilities():
+    """
+    Gets the associated liabilities held with the client contract 
+    Run a while loop to collect valid data from the user 
+    via the terminal, which much be a float. The loop will 
+    repeatedly request data until the input is valid 
+    """
+    print("Please enter the liabilities held with the client.")
+    print("data should be in number format, no need to add '%'")
+    print("Example: 2.5")
+
+    company_liabilities = input('Enter Client Burdens here: ')
+
+    if validate_liabilities(company_liabilities):
+        print("Data input valid \n")
+        break
+    return company_liabilities
+
+
+# Call the functions to start data collection
+def main():
+    """
+    Main function holding queue of function for calculating margins
+    """
+    get_bill_rate()
+    get_contract_duration()
+    get_burdens()
+    get_liabilities()
+
+main()
